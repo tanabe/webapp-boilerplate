3de8706f5f31656c42ea8b477556f4bc246570c1
Hideaki Tanabe <tanablog@gmail.com>
2018-10-20 08:49:35 +0900

    dev server settings

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,9 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    port: 9000,
+    contentBase: path.resolve(__dirname, './dist')
   }
 };
